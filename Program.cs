cb6cc4b9069d4ae1b6dbaf910bc5a1fe021cd5f0
Ben Watt <77812385+continualWolf@users.noreply.github.com>
2022-09-29 05:00:06 -0700

    InitialSetUp
    I have a test image and i have broke it down to pixels and their RGB values

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+//image
+var myBitmap = new Bitmap("C:\\Users\\Benjamin\\source\\repos\\ImageClassification\\testImg.png");
+
+//get the RGB values of each pixel
+for (int x = 0; x < myBitmap.Width; x++)
+{
+    for (int y = 0; y < myBitmap.Height; y++)
+    {
+        // Get the color of a pixel within myBitmap.
+        Color pixelColor = myBitmap.GetPixel(x, y);
+        string pixelColorStringValue =
+            pixelColor.R.ToString("D3") + " " +
+            pixelColor.G.ToString("D3") + " " +
+            pixelColor.B.ToString("D3") + ", ";
+        Console.WriteLine(pixelColorStringValue);
+        switch (pixelColorStringValue)
+        {
+            case "255 255 255":
+                {
+                    // white pixel
+                    break;
+                }
+            case "000 000 000":
+                {
+                    // black pixel
+                    break;
+                }
+        }
+    }
+}
